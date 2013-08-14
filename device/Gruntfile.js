b0e29d4a5dc230fc5faff6ab9f82bf7cb02f1007
bobby <bobby@sonicmoov.com>
2013-08-15 00:12:32 +0900

    と思ったがgruntでuglifyつかう

---
+++ device/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // ファイル結合の設定
+        concat: {
+            dist: {
+                src: [ '*.js', '!node_modules/*', '!output/*', '!Gruntfile.js' ],
+                dest: 'output/<%= pkg.name %>.js'
+            }
+        },
+
+        // ファイル圧縮の設定
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'output/<%= pkg.name %>.js',
+                dest: 'output/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // プラグインのロード
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // デフォルトタスクの設定
+    grunt.registerTask('build', [ 'concat', 'uglify' ]);
+
+};
